6baf2d00f52cb2c7197ea864aff42dfc176c404a
Fuad Makarim <m.fuadmakarim@gmail.com>
2023-01-12 16:59:50 +0700

    fix import layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { StateContext } from '../context/StateContext';
