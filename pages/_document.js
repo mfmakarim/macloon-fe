7c0d16359f5cf3729f69b37c1c1861afa17cfeb1
Fuad Makarim <m.fuadmakarim@gmail.com>
2023-01-16 20:59:57 +0700

    favicon and meta desc

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
+        <meta name="description" content="Macloon is ecommerce for artist to sell their artwork."></meta>
       </Head>
       <body>
         <Main />
